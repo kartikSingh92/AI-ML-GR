06f02c2de2abcb985ccc37ae537a69649d3c2375
Kartik Singh <kartik921363@gmail.com>
2026-09-23 09:55:28 +0530

    Add Day2 AI ML learning code

---
+++ Day2/practiceQuestions.py
@@ -0,0 +1,30 @@
+#Q1--> Write a python program to add two numbers.
+a = 10
+b = 30 
+sum = a+b
+print("Sum=",sum)
+
+#Q2--> Write a python program to find remainder when a number is divided by 2.
+num = 25
+remainder = num%2
+print("Remainder =",remainder)
+
+#Q3--> Check the type of variable assigned using input ()functon.
+a=input("Enter Something:")
+print("Type of variable:",type(a))
+
+#Q4--> Use comparison operator to find out whether a given variable a is greater than 'b' or not.Take a=34 and b=80
+a=34
+b=64
+print(a>b)
+#Q5--> Write a python program to find an average of two numbers entered by the user.
+a = float(input("Enter first number:"))
+b = float(input("Enter second number:"))
+average=(a+b)/2
+print("Average=",average)
+
+#Q6--> Write a python program to calculate the square of a number entered by the user.
+num = float(input("Enter a number:"))
+square=num**2
+print("Square=",square)
+
